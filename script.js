a80431d47da31314a0e937b766ba8770fa2763ce
profzampiva <168486632+profzampiva@users.noreply.github.com>
2024-08-05 09:27:34 -0300

    .

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector (".caixa-principal");
+const caixaPrincipal = document.querySelector (".caixa-perguntas");
+const caixaPrincipal = document.querySelector (".caixa-alternativas");
+const caixaPrincipal = document.querySelector (".caixa-resultados");
+const caixaPrincipal = document.querySelector (".texto-resultado");
+
+const perguntas = [
+    enunciado: "Perguntas 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+
+    "Perguntas 2"
+    
+];
